ac8eebbd123b8d787611b75ae3081d10a085f5c6
Kunal Malhotra <kunal.malhotra@idemia.com>
2022-01-13 00:35:07 +0530

    Publishing String Message

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # kafka-publisher
+
+https://archive.apache.org/dist/kafka/1.0.1/kafka_2.12-1.0.1.tgz
+
+C:\kafka_2.11-1.0.1\bin\windows
+
+
+start zookeeper
+zookeeper-server-start.bat C:\kafka_2.11-1.0.1\config\zookeeper.properties
+
+start kafkaserver
+kafka-server-start.bat C:\kafka_2.11-1.0.1\config\server.properties
+
+Create Topic
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 -topic TravelSeries
+
+Output: Created topic "TravelSeries".
+
+
+Run the application:
+http://localhost:9091/publish/viewers
+
+In case of error: Add this line below to C:\kafka_2.11-1.0.1\config\server.properties
+listeners=PLAINTEXT://127.0.0.1:9092
+
+After Running the above url:
+Check in the consumers console , whether the data is published or not
+
+kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic TravelSeries
+
+![Data Published](src/main/resources/img/Image1.JPG)
+
+http://localhost:9091/publish/Kunal
+![Data Published](src/main/resources/img/Image2.JPG)
+
+Whatever msg you publish on the topic, same will be displayed on the console above for 
+that topic.
--- src/main/java/com/kunal/kafka/api/KafkaPublisherApplication.java
+++ src/main/java/com/kunal/kafka/api/KafkaPublisherApplication.java
@@ -1,13 +1,31 @@
 package com.kunal.kafka.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class KafkaPublisherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaPublisherApplication.class, args);
-	}
+    @Autowired
+    private KafkaTemplate<String, Object> template;
+
+    private String topic = "TravelSeries";
+
+    @GetMapping("/publish/{name}")
+    public String publishMessage(@PathVariable String name) {
+        //topic created as shown in readme.md
+        template.send(topic, "Hi " + name + ". Welcome to Kunal's gitHub");
+        return "Data published";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPublisherApplication.class, args);
+    }
 
 }
